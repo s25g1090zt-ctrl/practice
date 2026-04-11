4d73e2f6239c2b439cf3d18c848a0624d609331d
ibuki <s25G1090ZT@chibakoudai.jp>
2026-04-12 01:21:03 +0900

    新たに降順を追加．他の細かいところを調整．

--- day2/temper.c
+++ day2/temper.c
@@ -18,18 +18,40 @@ double highvalue(int size, double temperatures[]){
     return max;
 }
 
+void sort_descending(int size, double temperatures[], int tmp){
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size - 1; j++){
+            if(temperatures[j] < temperatures[j+1]){
+            tmp = temperatures[j];
+            temperatures[j] = temperatures[j+1];
+            temperatures[j+1] = tmp;
+            }
+        }
+        
+    }
+}
+
 int main(){
     int size = 0;
     double temperatures[100];//最大100日とする．
+    int tmp;
 
     printf("何日分のデータにしますか？（最大100日まで）\n");
     scanf("%d", &size);
 
     for(int i=0; i<size; i++){
         printf("温度を入力してください．");
+        printf("%dつ目:", i+1);
         scanf("%lf", &temperatures[i]);
     }
 
     printf("平均温度は，%.1fです．\n",average(size, temperatures));
-    printf("最高温度は，%fです．\n", highvalue(size, temperatures));
+    printf("最高温度は，%.1fです．\n", highvalue(size, temperatures));
+
+    sort_descending(size, temperatures, tmp);
+    printf("降順は，");
+    for(int i = 0; i <  size; i++){
+        printf("%.1f, ", temperatures[i]);
+    }
+    printf("です．");
 }
